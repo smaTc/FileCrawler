765d434fb879291dc741c395f2a9e40b5940a736
Martin Fitzke <m.fitzke@gmx.net>
2020-12-20 02:35:26 +0100

    gitignore updated; parameter handling and help output

--- FileCrawler.py
+++ FileCrawler.py
@@ -2,7 +2,6 @@ import sys,os,shutil,traceback
 from pathlib import Path
 
 def getFilesInPath(path,pattern,wantString):
-    #return list(Path(".").rglob("*.[tT][xX][tT]"))
     posixPaths = list(Path(path).rglob(pattern))
     if wantString:
         pathStrings = []
@@ -19,33 +18,45 @@ def extendToFullPath(path):
     os.chdir(currPath)
     return newPath
 
-print("FileCrawler")
 # Default Values
 newDir = "CrawledFiles"
+path = None
+fileType = None
 
-# Check args
-paths = sys.argv[1:]
-if len(paths) < 2 or len(paths) > 3:
-    print("Not enough (at least 2) or too many (more than 3) arguments")
+print("Running FileCrawler Script\n")
+args = sys.argv[1:]
+
+i = 0
+while i < len(args):
+    if args[i] == "-p" or args[i] == "-path":
+        path = args[i+1]
+    elif args[i] == "-t" or args[i] == "-type":
+        fileType = "*." + args[i+1]
+    elif args[i] == "-o" or args[i] == "-out":
+        newDir = args[i+1]
+    
+    i += 1
+
+if path is None or fileType is None:
+    print("Path or fileType argument missing. Please execute the script as follows:")
+    print("python FileCrawler.py -p PATH -t TYPE -o OUTPUT")
+    print("Example: python FileCrawler.py -p ~/Documents -t pdf -o Docs")
+    print("Note: -o is optional. Default directory is \"CrawledFiles\"")
     sys.exit()
-elif paths[0] == "" or paths[1] == "":
-    print("source path or filetype cannot be empty")
-    sys.exit()
-elif len(paths) == 3:
-    newDir = paths[2]
+    
 
 # Extend paths
-paths[0] = extendToFullPath(paths[0])
+path = extendToFullPath(path)
 
 # Get directory names
-fileDirSplitList = paths[0].split("/")
+fileDirSplitList = path.split("/")
 fileDir = fileDirSplitList[len(fileDirSplitList)-1]
 
 if os.path.isdir(newDir):
     shutil.rmtree(newDir)
 
 os.mkdir(newDir)
-copyTree = getFilesInPath(paths[0],paths[1],False)
+copyTree = getFilesInPath(path,fileType,False)
 
 print("Copying Files:")
 for c in copyTree:
