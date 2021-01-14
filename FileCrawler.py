afa2f9e28fa0a1ac0d309ca4b026a283e57b2f49
Martin Fitzke <m.fitzke@gmx.net>
2021-01-14 15:01:38 +0100

    bug fixes

--- FileCrawler.py
+++ FileCrawler.py
@@ -2,7 +2,11 @@ import sys,os,shutil,traceback
 from pathlib import Path
 
 def getFilesInPath(path,pattern,wantString):
-    posixPaths = list(Path(path).rglob(pattern))
+    #posixPaths = list(Path(path).rglob(pattern))
+    posixPaths = list()
+    for p in pattern:
+        posixPaths.extend(list(Path(path).rglob(p)))
+
     if wantString:
         pathStrings = []
         for p in posixPaths:
@@ -26,7 +30,7 @@ def helpMessage(err=""):
     print("(python) FileCrawler(.py)")
     print("(python) FileCrawler(.py) -p PATH -t TYPE -o OUTPUT  -> omit python and .py if executable")
     print("Example: python FileCrawler.py -p ~/Documents -t pdf -o Docs")
-    print("Note: -p, -t and -o are optional. Default values are: -p -> ./; -t pdf,epub,azw3; -o  \"CrawledFiles\"")
+    print("Note: -p, -t and -o are optional. Default values are: -p -> ./; -t pdf,epub,azw3 (comma separated); -o  \"CrawledFiles\"")
     
 
 # Default Values
@@ -42,16 +46,16 @@ while i < len(args):
     if args[i] == "-p" or args[i] == "-path":
         path = args[i+1]
     elif args[i] == "-t" or args[i] == "-type":
-        args[i+1] = args[i+1].replace(" ", "")
-        fileType = args[i+1].split(",")
-        #fileType = "*." + args[i+1]
+        fileType = args[i+1].replace(" ", "").split(",")
     elif args[i] == "-o" or args[i] == "-out":
         newDir = args[i+1]
     
     i += 1
 
-for f in fileType:
-    f = "*." + f
+i = 0
+while i < len(fileType):
+    fileType[i] = "*." + fileType[i]
+    i += 1
     
 if path is None or fileType is None:
     #print()
@@ -71,6 +75,13 @@ if os.path.isdir(newDir):
 
 os.mkdir(newDir)
 copyTree = getFilesInPath(path,fileType,False)
+
+"""
+copyTree = []
+for f in fileType:
+    print(f)
+    copyTree.append(getFilesInPath(path,f,False))
+"""
 
 print("Copying Files:")
 copied = 0
